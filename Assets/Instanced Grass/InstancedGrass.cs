4822544bea72e0419470c6695ed7460207ded219
BlakeBeMent <bementbl@msu.edu>
2024-04-13 15:37:48 -0400

    Instanced grass uses a compute buffer to calculate positions rather than calculating per vertex.

--- Assets/Instanced Grass/InstancedGrass.cs
+++ Assets/Instanced Grass/InstancedGrass.cs
@@ -19,24 +19,47 @@ public class InstancedGrass : MonoBehaviour
     RenderParams renderParams = new RenderParams();
     Material material;
 
-    static readonly int positionsId = Shader.PropertyToID("_Positions");
+    public ComputeShader computeShader;
+    ComputeBuffer positionsBuffer;
+
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        widthId = Shader.PropertyToID("_Width"),
+        densityId = Shader.PropertyToID("_Density");
 
     private void OnEnable()
     {
+        positionsBuffer = new ComputeBuffer(density, sizeof(float));
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        computeShader.SetFloat(widthId, width);
+        computeShader.SetInt(densityId, density);
+
+        int groups = Mathf.CeilToInt(density / 64f);
+        computeShader.Dispatch(0, groups, 1, 1);
+
         material = new Material(shader);
         renderParams.material = material;
-        material.SetFloat("_Width", width);
-        material.SetInt("_Density", density);
+        renderParams.worldBounds = new Bounds(Vector3.zero, new Vector3(width, 1, 1));
+        material.SetBuffer(positionsId, positionsBuffer);
     }
 
     private void OnDisable()
     {
-        Destroy(material);
-        material = null;
+        positionsBuffer.Release();
+        positionsBuffer = null;
     }
 
     private void Update()
     {
         Graphics.RenderMeshPrimitives(renderParams, mesh, 0, density);
     }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        // draw bounding box
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(renderParams.worldBounds.center, renderParams.worldBounds.size);
+    }
+    #endif
 }
--- Assets/Instanced Grass/InstancedGrass.shader
+++ Assets/Instanced Grass/InstancedGrass.shader
@@ -16,38 +16,40 @@ Shader "Unlit/InstancedGrass"
 
             #include "UnityCG.cginc"
 
-            float _Width;
-            int _Density;
+            uniform float _Width;
+            uniform int _Density;
+            StructuredBuffer<float> _Positions;
 
             struct VertexData
             {
                 float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
                 uint instanceID : SV_InstanceID;
             };
 
             struct v2f
             {
                 float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
             };
 
             float CalculatePosition(int id)
             {
-                return 2.0 * _Width * id / (_Density - 1.0) - _Width;
+                return _Width * id / (_Density - 1.0) - _Width * 0.5;
             }
 
             v2f VertexProgram (VertexData i)
             {
                 v2f o;
-                float xPos = CalculatePosition(i.instanceID);
-                unity_ObjectToWorld._m03_m13_m23_m33 = float4(xPos, 0, 0, 1);
-                unity_ObjectToWorld._m00 = 0.02;
+                unity_ObjectToWorld._m03 = _Positions[i.instanceID];
                 o.vertex = UnityObjectToClipPos(i.vertex);
+                o.uv = i.uv;
                 return o;
             }
 
             float4 FragmentProgram (v2f i) : SV_Target
             {
-                return float4(0, 1, 0, 1);
+                return float4(0, i.uv.y, 0, 1);
             }
             ENDCG
         }
